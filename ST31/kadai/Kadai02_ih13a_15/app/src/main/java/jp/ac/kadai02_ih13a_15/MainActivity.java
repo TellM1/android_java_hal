2cdff68f8d47904273175f77b6ed714816a4cf76
TellM1 <terumi.game.0501@gmail.com>
2022-05-13 12:39:38 +0900

    update Study class 5_13

--- ST31/kadai/Kadai02_ih13a_15/app/src/main/java/jp/ac/kadai02_ih13a_15/MainActivity.java
+++ ST31/kadai/Kadai02_ih13a_15/app/src/main/java/jp/ac/kadai02_ih13a_15/MainActivity.java
@@ -10,5 +10,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button sampleBtn = findViewById(R.id.button);
+        sampleBtn.setOnClickListerner(view -> {
+            //クリック時の操作について
+            calcBMIs();
+        });
+    }
+    //    BMI計算についての記述　calucBMIs()
+    private void calcBMIs(){
+        //edit textの中身を習得して計算をする
     }
 }
