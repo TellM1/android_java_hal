98d4bef7fba791734b85e5a3a30be8fbb564fdf3
TellM1 <terumi.game.0501@gmail.com>
2022-05-13 09:45:28 +0900

    new java files

---
+++ ST31/kadai/Kadai02_ih13a_15/app/src/main/java/jp/ac/kadai02_ih13a_15/MainActivity.java
@@ -0,0 +1,14 @@
+package jp.ac.kadai02_ih13a_15;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
